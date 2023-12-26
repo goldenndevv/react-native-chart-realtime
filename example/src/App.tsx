90503548a9212025a2c4c3e9e0eddc88675d298d
goldenndevv <samson.adler.dev@gmail.com>
2023-12-26 11:42:48 +0700

    chore: run tests 1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-chat-realtime';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    setResult(1 + 1);
   }, []);
 
   return (
